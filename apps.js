b3799268a23bd14080841e96199034386dbef0eb
johnfrancesalcano <johnfrancesalcano@gmail.com>
2022-11-10 17:44:08 +0800

    SECTION 12, VIDEO 268, PRACTICE DAY

--- apps.js
+++ apps.js
@@ -1,1 +1,7 @@
-document.body.children[1].children[0].href =
+// document.body.children[1].children[0].href = 'https://google.com';
+
+let anchorElement = document.getElementById('external-method');
+anchorElement.href = 'https://google.com';
+
+anchorElement = document.querySelector('p a'); // a { color: red; }
+anchorElement.href = 'https://academind.com';
