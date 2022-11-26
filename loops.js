d596daa71f8a31124cf5e1dddc3d7bc2aaef6c57
johnfrancesalcano <johnfrancesalcano@gmail.com>
2022-11-26 16:55:06 +0800

    SECTION 13, VIDEO 298

---
+++ loops.js
@@ -0,0 +1,29 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+const users = ['Paran', 'Angel', 'Wewets'];
+// console.log(users);
+
+for (const user of users) {
+    console.log(user);
+}
+
+const loggedInUser = {
+    name: 'Paran',
+    age: 25,
+    isAdmin: true,
+};
+
+for (const propertyName in loggedInUser) {
+    console.log(propertyName);
+    console.log(loggedInUser[propertyName]);
+}
+
+let isFinished = false;
+
+while (!isFinished) {
+    isFinished = confirm('Do you want to quit?');
+};
+
+console.log('Done!');
