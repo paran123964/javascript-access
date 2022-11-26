0a83f38b4f29837859e4feeedaf4fbc44246bb41
johnfrancesalcano <johnfrancesalcano@gmail.com>
2022-11-26 14:07:10 +0800

    SECTION 13, VIDEO 292

--- conditional-code.js
+++ conditional-code.js
@@ -3,3 +3,15 @@ const myName = 'Paran';
 if (myName === 'Paran') {
     console.log('Hello');
 }
+
+const isLoggedIn = true;
+
+if (isLoggedIn === true) {
+    console.log('User is NOT logged in!');
+}
+
+const enteredUserName = 'Paran';
+
+if(enteredUserName) {
+    console.log('Input is valid!');
+}
